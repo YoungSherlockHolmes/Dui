509001dadd6cec54b8bd0c5cf504208820e12bad
echo <626097912@qq.com>
2017-01-17 16:34:39 +0800

    .

--- app/common/palaceNavigation.js
+++ app/common/palaceNavigation.js
@@ -4,7 +4,7 @@
  * 编写日期：2017年01月11日
  * 版权所有：湖南速微消费科技有限公司
  * 个人官网：http://www.051z.cc
- * 开源地址：https://github.com/YoungSherlockHolmes/Duidemo.git
+ * 开源地址：https://github.com/YoungSherlockHolmes/Dui
  */
 import React, { PropTypes } from 'react';
 import {
