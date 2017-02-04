3e27fdc80ec0ffdb2a01ca943aa38a31d84d3017
echo <626097912@qq.com>
2017-02-04 17:35:00 +0800

    完善个人中心

--- app/mainLayer/zonePage.js
+++ app/mainLayer/zonePage.js
@@ -14,9 +14,11 @@ import {
   Image,
   Dimensions,
   ScrollView,
+  TouchableOpacity
 } from 'react-native';
 import BaseComponent from '../base/baseComponent';
 var {height, width} = Dimensions.get('window');
+import Icon from 'react-native-vector-icons/Entypo';
 
 class ZonePage extends BaseComponent {
   constructor(props) {
@@ -33,8 +35,94 @@ class ZonePage extends BaseComponent {
     return (
       <View style={styles.container}>
         <ScrollView style={{ flex: 1 }} showsVerticalScrollIndicator={false}>
-        <View style={{backgroundColor:'yellow',height:300}}></View>
-
+          {/**头像账号 */}
+          <View style={styles.individual_account}>
+            <TouchableOpacity
+              activeOpacity={1}
+              style={{ marginTop: 20 }}
+              >
+              <Image style={styles.individual_account_img} source={require('../image/tutu.jpg')} />
+              <View style={styles.Photograph}>
+                <Icon name="camera" size={16} color="#272822" />
+              </View>
+            </TouchableOpacity>
+            <View style={styles.Account_name}>
+              <View style={styles.Membership_level}>
+                <Icon name="vimeo" size={18} color="#F7C603" />
+              </View>
+              <Text style={styles.Account_name_tx}>Dui会员</Text>
+            </View>
+          </View>
+          {/***列表 */}
+          <View style={styles.list_core}>
+            <View style={styles.list_core_abroad}>
+              <TouchableOpacity
+                activeOpacity={0.6}
+                style={styles.list_core_within}>
+                <View style={styles.list_core_lf}>
+                  <Icon name="v-card" size={25} color="#49A3F8" />
+                  <Text style={styles.list_core_lf_tx}>个人资料</Text>
+                </View>
+                <Icon name="chevron-thin-right" size={22} color="#BFBFBC" />
+              </TouchableOpacity>
+              <View style={styles.list_core_border}></View>
+            </View>
+          </View>
+          <View style={styles.list_core}>
+            <View style={styles.list_core_abroad}>
+              <TouchableOpacity
+                activeOpacity={0.6}
+                style={styles.list_core_within}>
+                <View style={styles.list_core_lf}>
+                  <Icon name="typing" size={25} color="#EAA497" />
+                  <Text style={styles.list_core_lf_tx}>消息中心</Text>
+                </View>
+                <Icon name="chevron-thin-right" size={22} color="#BFBFBC" />
+              </TouchableOpacity>
+              <View style={styles.list_core_border}></View>
+            </View>
+          </View>
+          <View style={styles.list_core}>
+            <View style={styles.list_core_abroad}>
+              <TouchableOpacity
+                activeOpacity={0.6}
+                style={styles.list_core_within}>
+                <View style={styles.list_core_lf}>
+                  <Icon name="sina-weibo" size={25} color="#FC786F" />
+                  <Text style={styles.list_core_lf_tx}>绑定微博</Text>
+                </View>
+                <Icon name="chevron-thin-right" size={22} color="#BFBFBC" />
+              </TouchableOpacity>
+              <View style={styles.list_core_border}></View>
+            </View>
+          </View>
+          <View style={styles.list_core}>
+            <View style={styles.list_core_abroad}>
+              <TouchableOpacity
+                activeOpacity={0.6}
+                style={styles.list_core_within}>
+                <View style={styles.list_core_lf}>
+                  <Icon name="image-inverted" size={25} color="#0C8244" />
+                  <Text style={styles.list_core_lf_tx}>我的相册</Text>
+                </View>
+                <Icon name="chevron-thin-right" size={22} color="#BFBFBC" />
+              </TouchableOpacity>
+              <View style={styles.list_core_border}></View>
+            </View>
+          </View>
+          <View style={styles.list_core}>
+            <View style={styles.list_core_abroad}>
+              <TouchableOpacity
+                activeOpacity={0.6}
+                style={styles.list_core_within}>
+                <View style={styles.list_core_lf}>
+                  <Icon name="star-outlined" size={25} color="red" />
+                  <Text style={styles.list_core_lf_tx}>我的收藏</Text>
+                </View>
+                <Icon name="chevron-thin-right" size={22} color="#BFBFBC" />
+              </TouchableOpacity>
+            </View>
+          </View>
         </ScrollView>
       </View>
     );
@@ -53,6 +141,47 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#EFEFEF',
   },
+  individual_account: {
+    backgroundColor: '#5CB85C', height: 180, alignItems: 'center',
+  },
+  individual_account_img: {
+    width: 90, height: 90, borderRadius: 90, borderColor: 'white', borderWidth: 3
+  },
+  Account_name: {
+    marginTop: 10, alignItems: 'center', flexDirection: 'row'
+  },
+  Account_name_tx: {
+    fontSize: 16, color: 'white'
+  },
+  Photograph: {
+    position: 'absolute', right: 0, bottom: 5, backgroundColor: 'white',
+    width: 24, height: 24, borderRadius: 24, alignItems: 'center',
+    justifyContent: 'center',
+  },
+  list_core: {
+    backgroundColor: 'white', width: width
+  },
+  list_core_abroad: {
+    marginLeft: 15, marginRight: 15
+  },
+  list_core_within: {
+    paddingTop: 12, paddingBottom: 12, flexDirection: 'row', alignItems: 'center', justifyContent: 'space-between'
+  },
+  list_core_lf: {
+    flexDirection: 'row'
+  },
+  list_core_lf_tx: {
+    fontSize: 16, color: '#272822', paddingLeft: 8
+  },
+  list_core_border: {
+    borderBottomColor: '#BFBFBC', borderBottomWidth: 1
+  },
+  Membership_level: {
+    backgroundColor: 'yellow', width: 20, height: 20,
+    justifyContent: 'center', alignSelf: 'center', borderRadius: 20, marginRight: 5
+  },
+
+
 });
 
 export default ZonePage;
